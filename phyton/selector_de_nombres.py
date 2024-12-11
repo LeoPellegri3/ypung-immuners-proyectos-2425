2d59a575ab9cf1486be5643a18aa487f99b78448
LeoPellegri3 <pellegrileo@gmail.com>
2024-12-11 19:03:57 +0100

    selector de nombres

---
+++ phyton/selector_de_nombres.py
@@ -0,0 +1,9 @@
+claseA = ["Alai, Leo, Claudia, Raul, Saul, Maria, Veronica, Alvaro, Mario, Iago"]
+
+longitud = len(claseA)
+
+import random
+
+numero_aleatorio = random.randint (0, longitud - 1)
+
+print(f"El afortunado ganador de la loteria de diciembre 2024 es: {numero_aleatorio}")
